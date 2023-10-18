c362efab013836e9c42911d63cddfa84d8649d0e
VIJAYaraaghavan Manoharan <AM.Vijay@gmail.com>
2023-10-18 12:28:09 +0000

    aws serverless lambda example

--- aws-svrls-lf-game-voting/src/index.ts
+++ aws-svrls-lf-game-voting/src/index.ts
@@ -1,9 +1,13 @@
-export async function handler(event: any) {
-    console.log("Hello, World");
+import { Callback, Context } from 'aws-lambda';
+
+export async function handler(event: any, context: Context, callback: Callback) {
+    console.log("event", event);
+    const item: GameVoting = event;
+    console.log("item ", item);
     return "Hello, World";
 }
 
-interface GameVoting{
+interface GameVoting {
     gameId: string;
     customerId: string;
     votes: string;
